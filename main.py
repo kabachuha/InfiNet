b76578f24a204b93ac18d400c7a6a6177e069822
kabachuha <artemkhrapov2001@yandex.ru>
2023-03-29 18:14:11 +0300

    don't really need torch 2 checks

--- main.py
+++
@@ -1,4 +0,0 @@
-import torch
-if not torch.__version__.startswith('2'):
-    print("Please, use torch 2 when training, or else you'll lose a lot of compute :)")
-    exit()
--- setup_torch_2.sh
+++
@@ -1,2 +0,0 @@
-pip3 install numpy --pre torch torchvision torchaudio --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu117
- 
